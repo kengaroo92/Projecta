b4ceae1a14b964c3a973c1bbd4e86c5534a02d10
Kenan Bjelosevic <kenanbjelosevic92@gmail.com>
2023-04-02 12:09:35 -0600

    Added CRUD operations to CustomersController

--- Projecta.API/Controllers/CustomersController.cs
+++ Projecta.API/Controllers/CustomersController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Projecta.Core.Entities;
 using Projecta.Core.Repositories;
 
 namespace Projecta.API.Controllers
@@ -12,6 +14,71 @@ namespace Projecta.API.Controllers
         public CustomersController(ICustomerRepository customerRepository)
         {
             _customerRepository = customerRepository;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var customers = await _customerRepository.GetAllAsync();
+            return Ok(customers);
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var customer = await _customerRepository.GetByIdAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+        // POST: api/Customers
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _customerRepository.AddAsync(customer);
+            return CreatedAtAction(nameof(GetById), new { id = customer.Id }, customer);
+        }
+
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            await _customerRepository.UpdateAsync(customer);
+            return NoContent();
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var customer = await _customerRepository.GetByIdAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            await _customerRepository.DeleteAsync(id);
+            return NoContent();
         }
     }
 }
