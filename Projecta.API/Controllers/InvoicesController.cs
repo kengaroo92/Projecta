12657fd6af9a7327b3e0531eea467e29d21838f4
Kenan Bjelosevic <kenanbjelosevic92@gmail.com>
2023-04-02 12:36:12 -0600

    Added CRUD operations to InvoicesController

--- Projecta.API/Controllers/InvoicesController.cs
+++ Projecta.API/Controllers/InvoicesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Projecta.Core.Entities;
 using Projecta.Core.Repositories;
 
 namespace Projecta.API.Controllers
@@ -12,6 +14,71 @@ namespace Projecta.API.Controllers
         public InvoicesController(IInvoiceRepository invoiceRepository)
         {
             _invoiceRepository = invoiceRepository;
+        }
+
+        // GET: api/Invoices
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var invoices = await _invoiceRepository.GetAllAsync();
+            return Ok(invoices);
+        }
+
+        // GET: api/Invoices/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var invoice = await _invoiceRepository.GetByIdAsync(id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+            return Ok(invoice);
+        }
+
+        // POST: api/Invoices
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Invoice invoice)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _invoiceRepository.AddAsync(invoice);
+            return CreatedAtAction(nameof(GetById), new { id = invoice.Id }, invoice);
+        }
+
+        // PUT: api/Invoices/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Invoice invoice)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != invoice.Id)
+            {
+                return BadRequest();
+            }
+
+            await _invoiceRepository.UpdateAsync(invoice);
+            return NoContent();
+        }
+
+        // DELETE: api/Invoices/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var invoice = await _invoiceRepository.GetByIdAsync(id);
+            if (invoice == null)
+            {
+                return NotFound();
+            }
+
+            await _invoiceRepository.DeleteAsync(id);
+            return NoContent();
         }
     }
 }
